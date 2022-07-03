8411d97d5f4d8f20e401a72bc6bf767f2d320fe0
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-03 18:19:35 +0000

    個数追加機能の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,12 +7,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     # binding.pry
-    # item = Item.find(params[:cart_item][:item_id])
-    # puts cart_item_params[:item_id]
-    # puts cart_item_params[:amount]
-    cart_item = current_customer.cart_items.new(cart_item_params)
-    if cart_item.save
+    # item = Item.find_by(id: cart_item_params[:item_id])
+    # customer = CartItem.find_by(customer_id: current_customer, item_id: cart_item_params[:item_id])
+    item = CartItem.find_by(customer_id: current_customer, item_id: cart_item_params[:item_id])
+    if item
+      cart_item = item.update(amount: item.amount + cart_item_params[:amount].to_i)
       redirect_to cart_items_path
+    else
+      cart_item = current_customer.cart_items.new(cart_item_params)
+      if cart_item.save
+        redirect_to cart_items_path
+      end
     end
   end
 
